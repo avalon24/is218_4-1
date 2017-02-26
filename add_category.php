9a95b98d0582c7e6dc026a11d301129e6646edbd
ankan basu <ab936@afsconnect1.njit.edu>
2017-02-26 16:22:21 -0500

    Removed the auto increment item from insertion

--- add_category.php
+++ add_category.php
@@ -1,11 +1,10 @@
 <?php
 // Get the product data
-$category_id = filter_input(INPUT_POST, 'category_id', FILTER_VALIDATE_INT);
+//$category_id = filter_input(INPUT_POST, 'category_id', FILTER_VALIDATE_INT);
 $category_name = filter_input(INPUT_POST, 'category_name');
 
 // Validate inputs
-if ($category_id == null || $category_id == false ||
-        $category_name == null) {
+if ($category_name == null) {
     $error = "Invalid product data. Check all fields and try again.";
     include('error.php');
 } else {
@@ -13,16 +12,15 @@ if ($category_id == null || $category_id == false ||
 
     // Add the product to the database  
     $query = 'INSERT INTO categories_guitar1
-                 (categoryID, categoryName)
+                 (categoryName)
               VALUES
-                 (:category_id, :category_name)';
+                 (:category_name)';
     $statement = $db->prepare($query);
-    $statement->bindValue(':category_id', $category_id);
     $statement->bindValue(':category_name', $category_name);
     $statement->execute();
     $statement->closeCursor();
 
     // Display the Product List page
-    include('index.php');
+    include('category_list.php');
 }
 ?>
